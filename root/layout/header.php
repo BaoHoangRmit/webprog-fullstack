a94a02fa9c1ba7e5735dbfdd1939c65a9839fa0d
Bao Hoang Rmit <s3926050@rmit.edu.vn>
2022-09-08 13:51:12 +0700

    test add product folder

--- root/layout/header.php
+++ root/layout/header.php
@@ -4,16 +4,16 @@
     session_start();
 
     if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) {
-        echo 'ok' . 'login van return $_SESSION[current_user] voi mang rong -> van chuyen my account khi an quay lai sau khi dang nhap sai mkhau';
-        echo '<br>';
-        echo 'test above function again but for now have fixed';
+        // echo 'ok' . 'login van return $_SESSION[current_user] voi mang rong -> van chuyen my account khi an quay lai sau khi dang nhap sai mkhau';
+        // echo '<br>';
+        // echo 'test above function again but for now have fixed';
         if (isset($_SESSION['current_user'])) {
 		    echo '<pre>';
 		    print_r($_SESSION['current_user']);
 		  	echo '</pre>';
 		}
     } else {
-        echo 'not ok';
+        // echo 'not ok';
     }
 
     // if (isset($_SESSION['current_user'])) {
---
+++ root/test/test_product_add/product-validation.php
@@ -0,0 +1,26 @@
+<?php 
+
+include_once 'product-file-control.php';
+
+function check_product_name($p1) {
+    $product_name_pattern = "/^([a-zA-Z0-9 !@#$%^&*]+){10,20}$/";
+    if (preg_match($product_name_pattern, strval($p1))) {
+        return 1;
+    } else return 0;
+}
+
+function check_product_price($p1) {
+    $product_price_pattern = "/^[+]?\d+([.]\d+)?$/";
+    if (preg_match($product_price_pattern, strval($p1))) {
+        return 1;
+    } else return 0;
+}
+
+function check_product_desc($p1) {
+    $product_desc_pattern = "/^([a-zA-Z0-9 !@#$%^&*]+){1,500}$/";
+    if (preg_match($product_desc_pattern, strval($p1))) {
+        return 1;
+    } else return 0;
+}
+
+?>
